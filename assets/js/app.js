69d80148c891110ef35505ca030599d9833b5694
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-02-28 12:06:13 +0530

    footer update

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,46 @@ $('.slick-slider').slick({
         },
     ]
 });
+
+let scroll_top = document.querySelector(".arrow");
+let scrollPt = 200;
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > scrollPt) {
+        scroll_top.style.display = "block";
+    } else {
+        scroll_top.style.display = "none";
+    }
+})
+
+scroll_top.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+
+
+
+gsap.from(".arrow", {
+    y: 30,
+    repeat: -1,
+    yoyo: true
+})
+
+
+// let mode = document.querySelector("body");
+// let light = document.querySelector(".moon");
+// let modee = "light";
+// light.addEventListener("click", () => {
+
+//     if (modee === "light") {
+//         mode.style.backgroundColor = "black";
+//         modee = "dark";
+//     } else {
+//         mode.style.backgroundColor = "white";
+//         modee = "light";
+//     }
+// })
